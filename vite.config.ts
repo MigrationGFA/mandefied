bc5da280e40e5f76b8f4d215b6e61036c6c7bde2
Evans Diegebe <devans3001@gmail.com>
2026-04-24 13:41:57 +0100

    pls work

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
+import viteReact from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  vite: {
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-    nitro: {
+  plugins: [
+    tanstackStart(),
+    nitro({
+      // This is where we tell Nitro to build for Vercel
       preset: "vercel",
+    }),
+    viteReact(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
